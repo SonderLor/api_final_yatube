abfd264708a8038b68369752a00a093e57afa5f4
Aleksei Konstantinov <mralexey111@gmail.com>
2025-02-26 01:42:45 +0300

    add Follow model

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -44,3 +44,12 @@ class Comment(models.Model):
     created = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True
     )
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='followings'
+    )
+    following = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='followers'
+    )
