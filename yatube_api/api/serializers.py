2c67f4a98457b9623aea7880ccc1d0408f27b085
Aleksei Konstantinov <mralexey111@gmail.com>
2025-02-26 01:50:23 +0300

    create GroupSerializer

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 
-from posts.models import Comment, Post
+from posts.models import Comment, Post, Group
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+
+    class Meta:
+        fields = ("id", "title", "slug", "description")
+        model = Group
+        read_only_fields = ("id",)
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -21,4 +32,4 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ("id", "author", "text", "created", "post")
         model = Comment
-        read_only_fields = ("text",)
+        read_only_fields = ("id", "author", "created", "post")
